b5b000ba0a33e843d0f612146ee27be4aba748bb
Sunny Arora <sunny@fasterhq.com>
2026-07-04 04:56:38 +0000

    faster 0.0.62

--- Formula/faster.rb
+++ Formula/faster.rb
@@ -1,9 +1,9 @@
 class Faster < Formula
   desc "Chat-first terminal client for a Faster box"
   homepage "https://github.com/FasterHQ/faster-console"
-  url "https://github.com/FasterHQ/homebrew-tap/releases/download/faster-v0.0.61/faster-0.0.61-macos-universal.tar.gz"
-  sha256 "c16d0dfb3468ad01f26bb127571affc8ecc546db1ed03616eb876bd63a0da51a"
-  version "0.0.61"
+  url "https://github.com/FasterHQ/homebrew-tap/releases/download/faster-v0.0.62/faster-0.0.62-macos-universal.tar.gz"
+  sha256 "2ad4d9f4330359725f0afe210fecffab6516413dc750db11e286a3a98e3d21ce"
+  version "0.0.62"
 
   def install
     bin.install "faster"
